kerwizzy/ZappWiki

commit d0b2a2000372610e184f26c8691180eea4ab9035

Changed css to improve display on small screens.

core/default.css
@@ -9,11 +9,7 @@ border-radius:5px;
 border: 1px solid gray;
 }
 
-.wikieditor-body{
-padding: 12px;
-overflow: auto;
-resize: vertical;
-}
+
 
 .wikieditor-toolbar{
 	background-color:#DDD;
@@ -34,10 +30,6 @@ margin-top:10px;
 
 /*PAGE STYLES*/
 
-.editMenu {
-margin-bottom:4px;
-}
-
 .pagetitle {
 font-size:50px;
 font-weight:bold;
@@ -46,14 +38,6 @@ font-weight:bold;
 .CodeMirror {
   border: 1px solid #eee;
   height: 80vh;
-}
-
-.sidebar {
-max-height:100vh;
-direction:rtl;
-padding-left:15px;
-overflow-y:auto;
-overflow-x:hidden;
 }
 
 .expandSidebar {
@@ -74,12 +58,6 @@ background-color:rgba(0,0,0,0);
 border:none;
 }
 
-.wideBody {
-width:95%;
-margin-left:5%;
-padding:5px;
-}
-
 input.headerInput {
 background-color: white;
 	box-shadow: 0px 0px 20px #DDD inset;
@@ -92,10 +70,6 @@ overflow-y:auto;
 max-height:100vh;
 }
 
-body {
-max-height:100vh;
-overflow:hidden;
-}
 
 .isResizable {
   overflow: hidden;

core/template.html
@@ -68,26 +68,8 @@ background-color:#333;
 	</div>
 </div>
 -->
-<div class='col-md-5 col-md-offset-3' id='bodyWrapper'>
 
-<div id='body'>
-</div>
-
-<BR>
-<BR>
-<BR>
-<BR>
-<BR>
-<BR>
-<BR>
-<BR>
-<BR>
-<BR>
-
-
-
-</div>
-<div id='sidebarWrapper' class='col-md-4'>
+<div id='sidebarWrapper' class='col-md-4 col-md-push-6 col-md-offset-2'>
 <div class="panel-group" id="sidebar" role="tablist">
 	<div class="panel panel-primary">
 		<div class="panel-heading" role="tab">
@@ -174,7 +156,25 @@ background-color:#333;
 	</div>
 </div>
 </div>
+<div class='col-md-6 col-md-pull-4' id='bodyWrapper'>
 
+<div id='body'>
+</div>
+
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+<BR>
+
+
+
+</div>
 </div>
 
 <div class="modal fade" id='settingsModal' tabindex="-1" role="dialog">

commit d0a048460c196102ad664cd21402b3c8ad5707df

Fixed WYSIWYG resizing, which was broken by mistake in d0b2a20.

core/client.js
@@ -210,6 +210,7 @@ var Wiki = {
 			document.getElementById("texteditwrapper").innerHTML = ""
 			
 			Wiki.wysiwygEditor = new Wiki.editor(document.getElementById("texteditwrapper"),html)
+			Wiki.wysiwygEditor.enableResize()
 			Wiki.wysiwygEditor.addStandardButtons();
 			Wiki.wysiwygEditor.height = height
 			

core/default.css
@@ -4,6 +4,11 @@
 
 }
 
+.wikieditor-body{
+padding: 12px;
+overflow: auto;
+}
+
 .wikieditor-wrapper{
 border-radius:5px;
 border: 1px solid gray;
@@ -89,15 +94,15 @@ max-height:100vh;
 /* POPDOWN STYLES */
 
 @keyframes popdownAction {
-	0% {right:-200px;}
-    20%  {right:0px;}
-	80%  {right:0px;}
-    100% {right:-200px;}	
+	0% {left:-200px;}
+    20%  {left:0px;}
+	80%  {left:0px;}
+    100% {left:-200px;}	
 }
 
 .popdown-animation {
 	top:0;
-	right:-200px;	
+	left:-200px;	
 	animation-name:popdownAction;
 	animation-duration:4s;	
 }
@@ -107,12 +112,12 @@ max-height:100vh;
 	color:black;
 	border:1px solid gray;
 	border-radius:2px;
-	box-shadow:5px 5px 2px black;
+	box-shadow:5px 5px 2px rgba(0,0,0,0.7);
 	background-color:white;
 	height:30px;
 	position:fixed;
 	padding:5px 10px;
 	margin:5px 10px;
 	top:0;
-	right:-200px;
+	left:-200px;
 }

core/wysiwyg.js
@@ -19,9 +19,7 @@ Wiki.editor = class {
 	}
 	
 	enableResize() {
-		this.element.style.overflow = "hidden"
-		this.element.resize = "both"
-		this.element.border = "1px solid gray"
+		this.element.style.resize = "vertical"
 	}
 	
 	get html() {